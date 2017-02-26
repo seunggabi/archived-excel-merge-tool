d27ace9549743d386d45ee728eea15d3f6d5a2d3
seunggabi <seunggabi.kim@navercorp.com>
2017-02-26 20:34:35 +0900

    min, max, extendRange 추가

--- public/javascripts/excel-merge-tool.js
+++ public/javascripts/excel-merge-tool.js
@@ -4,19 +4,56 @@
 
 module.exports = {
 	mergeSheets: function(wb1, wb2) {
-		var wb = wb1;
-
-		var index = -1;
 		for(var s in wb2.Sheets) {
-			index++;
 			if(wb1.Sheets.hasOwnProperty(s)) {
-				continue;
+				wb1.Sheets[s] = this.mergeCells(wb1.Sheets[s], wb2.Sheets[s]);
 			} else {
 				wb1.Sheets[s] = wb2.Sheets[s];
-				wb1.SheetNames.push(wb2.SheetNames[index]);
+				wb1.SheetNames.push(s);
 			}
 		}
-
-		return wb;
+		return wb1;
 	},
+
+	mergeCells: function(s1, s2) {
+		for(var c in s2) {
+			if(s1.hasOwnProperty(c)) {
+				if(c === "!ref") {
+					this.extendsRange(s1[c], s2[c]);
+				}
+				s1[c].t = "s";
+				s1[c].v = s1[c].v+String.fromCharCode(13)+s2[c].v;
+			} else {
+				s1[c] = s2[c];
+			}
+		}
+		return s1;
+	},
+
+	extendsRange: function(r1, r2) {
+		var r;
+		var regRow = /\w+/g;
+		var regCol = /\d+/g;
+
+		var r1Row = r1.match(regRow);
+		var r1Col = r1.match(regCol);
+		var r2Row = r2.match(regRow);
+		var r2Col = r2.match(regCol);
+
+		r = this.min(r1Row[0], r2Row[0])
+			+ this.min(r1Col[0], r2Col[0])
+			+ ":"
+			+ this.max(r1Row[1], r2Row[1])
+			+ this.max(r1Col[1], r2Col[1]);
+
+		return r;
+	},
+
+	max: function(a, b) {
+		return a > b ? a : b;
+	},
+
+	min: function(a, b) {
+		return a < b ? a : b;
+	}
 };
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,15 @@ var router = express.Router();
 
 if(typeof require !== "undefined") XLSX = require("xlsx");
 
-var wb1 = XLSX.readFile("files/test.xlsx");
-var wb2 = XLSX.readFile("files/test2.xlsx");
-var wb3 = EMT.mergeSheets(wb1, wb2);
-XLSX.writeFile(wb3, "files/test3.xlsx");
-
 /* GET home page. */
 router.get("/", function(req, res, next) {
 	res.render("index", { title: "Excel Merge Tool" });
+
+	var wb1 = XLSX.readFile("files/test.xlsx");
+	var wb2 = XLSX.readFile("files/test2.xlsx");
+	var wb3 = EMT.mergeSheets(wb1, wb2);
+	XLSX.writeFile(wb3, "files/test3.xlsx");
+
 });
 
 module.exports = router;
