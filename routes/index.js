07d48857cc2b347fbdb1c48846b887596c731cf3
seunggabi <seunggabi.kim@navercorp.com>
2017-02-26 16:22:09 +0900

    writeFile test

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,11 @@ var router = express.Router();
 
 if(typeof require !== "undefined") XLSX = require("xlsx");
 var workbook = XLSX.readFile("files/test.xlsx");
+XLSX.writeFile("files/test2.xlsx");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-	console.dir(XLSX);
-	console.log(workbook);
-	res.render("index", { title: "Express" });
+	res.render("index", { title: "Excel Merge Tool" });
 });
 
 module.exports = router;
