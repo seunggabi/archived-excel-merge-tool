933692353c6bddd4046cadd4a8c2a86af7d4a79a
seunggabi <seunggabi.kim@navercorp.com>
2017-05-03 21:07:05 +0900

    worker load add

--- public/excel-merge-tool/excel-merge-tool-worker-write.js
+++ public/excel-merge-tool/excel-merge-tool-worker-write.js
@@ -2,16 +2,7 @@
  * Created by seunggabi on 2017. 4. 15..
  */
 
-importScripts("/lib/cpexcel.js");
-importScripts("/lib/jszip.js");
-importScripts("/lib/xlsx.js");
-
-importScripts("/excel-merge-tool/excel-merge-tool-config.js");
-importScripts("/excel-merge-tool/excel-merge-tool-data.js");
-importScripts("/excel-merge-tool/excel-merge-tool-log.js");
-importScripts("/excel-merge-tool/excel-merge-tool-statistics.js");
-importScripts("/excel-merge-tool/excel-merge-tool-util.js");
-importScripts("/excel-merge-tool/excel-merge-tool.js");
+importScripts("/excel-merge-tool/excel-merge-tool-worker-load.js");
 
 function callback(event) {
 	var options = event.data.options;
